7d889790ec06f87e0a6c8295a6cc8d40d8c11759
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2023-08-04 10:14:20 -0400

    Implementação do ContentProvider

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # App-Content-Provider-BootcampEveris
+
+### um app para estudar e treinar o Content Provider
+
+Esta é a documentação oficial do aplicativo [App Content Provider Bootcamp Everis]
+para o Bootcamp Everis Bootcamp.
+
+## Screenshots
+
+<img src="/" width=25%>  <img src="">
+
+## Tecnologias utilizadas
+
+O aplicativo foi desenvolvido utilizando as seguintes tecnologias:
+
+- Content Provider
+- Dialog
+- Android Studio
+- Kotlin
+- Android JetPack
+- Material Designer 3
+- RecyclerView + adapter
+
+## Instalação
+
+Para instalar o aplicativo, siga os seguintes passos:
+
+1. Clone o Repositorio na
+   maquina [aqui](https://github.com/JorgeTranin/App-GitHub-Search-Repositorys-AMVVM).
+2. Abra o Android Studio e execute o app
+
+## Como usar
+
+Para utilizar o aplicativo, siga os seguintes passos:
+
+1. Abra o aplicativo.
+2. Simule qual carro quer calcular a autonomia
+3. Adicione carros a sua coleção favoritos
+4. Calcule autonomia de cada carro eletrico
+
+## Contribuição
+
+Quem quiser fazer contribuições para o aplicativo! basta fazer as mudanças e criar um pull Requests
+
+1. Faça um fork deste repositório.
+2. Crie uma branch com sua nova funcionalidade: `git checkout -b my-new-feature`
+3. Faça suas alterações e faça commit: `git commit -m 'Add some feature'`
+4. Envie suas alterações para a branch principal: `git push origin my-new-feature`
+5. Abra um Pull Request e descreva suas alterações.
+
+## Autoria e Licença
+
+Este aplicativo foi desenvolvido por [Jorge Tranin] e é licenciado sob [].
+
--- app/src/main/java/com/jorgetranin/contentprovider/MainActivity.kt
+++ app/src/main/java/com/jorgetranin/contentprovider/MainActivity.kt
@@ -3,9 +3,9 @@ package com.jorgetranin.contentprovider
 import android.annotation.SuppressLint
 import android.database.Cursor
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.BaseColumns._ID
+import androidx.appcompat.app.AppCompatActivity
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
@@ -14,7 +14,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.jorgetranin.contentprovider.database.NotesDatabaseHelper.Companion.TITLE_NOTES
 import com.jorgetranin.contentprovider.database.NotesProvider.Companion.URI_NOTES
-import java.net.URI
 
 class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
     lateinit var noteRecyclerView: RecyclerView
@@ -27,12 +26,14 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
 
         noteAdd = findViewById(R.id.fab_add_Notes)
         noteAdd.setOnClickListener {
-
+            NoteDetailFragment().show(supportFragmentManager, "dialog")
         }
-        adapter = NotesAdapter(object : NoteClickListener{
+        adapter = NotesAdapter(object : NoteClickListener {
             @SuppressLint("Range")
             override fun noteClickItem(cursor: Cursor) {
                 val id = cursor.getLong(cursor.getColumnIndex(_ID))
+                val fragment = NoteDetailFragment.newInstance(id)
+                fragment.show(supportFragmentManager, "dialog")
             }
 
             @SuppressLint("Range")
@@ -46,19 +47,20 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
         noteRecyclerView = findViewById(R.id.rv_Notes)
         noteRecyclerView.layoutManager = LinearLayoutManager(this)
         noteRecyclerView.adapter = adapter
+        LoaderManager.getInstance(this).initLoader(0, null, this)
     }
 
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> =
         CursorLoader(this, URI_NOTES, null, null, null, TITLE_NOTES)
 
 
-    override fun onLoaderReset(loader: Loader<Cursor>, data: Cursor?) {
-        if (data != null){
-
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        if (data != null) {
+            adapter.setCursor(data)
         }
     }
 
-    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        TODO("Not yet implemented")
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        adapter.setCursor(null)
     }
 }
